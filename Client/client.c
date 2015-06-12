8f5c4bbcf8899707810ba640affcdccdc9b99f9d
yhkk2585 <yhkk2585@naver.com>
2015-06-12 15:01:01 +0900

    put implements

---
+++ Client/client.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <netdb.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+
+void printPercent(double percent, int sumfileSize, int fileSize, int *count);
+int main(int argc, char *argv[])
+{
+	struct hostent *hostp;
+	struct servent *servp;
+	struct sockaddr_in server_addr;
+	int sock;
+	static struct timeval timeout = { 5, 0 }; /* five seconds */
+	fd_set rmask, xmask, mask;
+	int nfound, bytesread;
+	int sockid, retcode, nread, retval, addrlen;
+	size_t numread;
+	int sumfileSize = 0;
+	int fileSize;
+	int count = 0;
+	double percent = 0;
+	char msg[255];
+	char input[255];
+	char buf[64];
+	char strComplete[10] = "complete";
+	char portNumber[20];
+	char command[20];
+	char file_name[50];
+	int randomPortNum = rand() % 1000;
+	int BUFSIZE = 63;
+	char strConnect[10] = "connect";
+	char strPut[10] = "put";
+	char strGet[10] = "get";
+	char strClose[10] = "close";
+	char ipAddress[20];
+	char strfileSize[20];
+	char strACK[10] = "ACK";
+	char strEmpty[10] = "Empty";
+	char nameAndSize[100];
+	char strFinish[50] = "finish";
+
+	// argv[1] = portNumber   argv[2] = localhost
+
+	while (1){
+		printf("Enter connect [portNumber] [hostAddress] \n"); // 처음 접속시의 출력문  connect localhost 10000 순으로 입력 
+		scanf("%s %s %s", input, portNumber, ipAddress); // enter connect 
+
+		if (strcmp(strConnect, input)) {
+			(void)fprintf(stderr, "please ");
+
+		}
+		else break;
+	}
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+
+	if (isdigit(portNumber[0])) { //,,if first is number
+		static struct servent s;
+		servp = &s;
+		s.s_port = htons((u_short)atoi(portNumber));
+
+	}
+	else if ((servp = getservbyname(portNumber, "tcp")) == 0) {
+
+		printf("********* isdigit pass \n");
+		fprintf(stderr, "%s: unknown service\n", portNumber);
+		exit(1);
+	}
+	if ((hostp = gethostbyname(ipAddress)) == 0) {
+		fprintf(stderr, "%s: unknown host\n", ipAddress);
+		exit(1);
+	}
+	memset((void *)&server_addr, 0, sizeof server_addr);
+	server_addr.sin_family = AF_INET;
+	memcpy((void *)&server_addr.sin_addr, hostp->h_addr, hostp->h_length);
+	server_addr.sin_port = servp->s_port;
+	if (connect(sock, (struct sockaddr *)&server_addr, sizeof server_addr) < 0) {
+		(void)close(sock);
+		perror("connect");
+		exit(1);
+	}
+	printf("********* connect pass \n");
+	FD_ZERO(&mask);
+	FD_SET(sock, &mask);
+	FD_SET(fileno(stdin), &mask);
+
+
+	while (1){
+
+		printf("----Enter your command.\n put [file_name] | get [file_name] | close\n");
+		scanf("%s", command);
+		if (!strcmp(strClose, command)){  // close 입력시 종료 
+
+			strcpy(msg, strClose);
+			retcode = write(sock, msg, 20);  //메세지 전송 
+
+			break; // 종료. 
+
+		}
+		scanf("%s", file_name);
+
+
+		////////////////////////////// put 
+
+
+		if (!strcmp(strPut, command)){  // put 입력시 
+
+			printf("----Put! %s \n", file_name);
+			FILE *fp = fopen(file_name, "r+");  // fp라는 파일을 읽고쓰기 형식으로 연다.
+
+			fseek(fp, 0, SEEK_END); //file size check 파일의끝을 참고하면서 파일의 사이즈를 구하는것임 
+			fileSize = ftell(fp);             //파일사이즈 fileSize 변수에 저장. 
+			printf("[%s] (size: %d MB)\n", file_name, fileSize / 1000000);
+			fseek(fp, 0, SEEK_SET); //다시 처음을 보게한다. 파일의끝을 보게하고있으면 전송이 안됨. 
+			sumfileSize = 0;
+			count = 0;
+			retcode = 1;
+
+			strcpy(msg, strPut); // put 커멘드를 server에 전송하여 이제 서버에서 recive받을 준비를 하라고 말해줌. 
+			retcode = write(sock, msg, 20);  //메세지 전송 
+
+			strcpy(nameAndSize, file_name); //
+			strcat(nameAndSize, ".");
+			sprintf(msg, "%d", fileSize);// 파일사이즈는 itoa 로 변환해서 전달 
+
+			printf("fileSize : %s \n", msg);
+			strcat(nameAndSize, msg);
+
+			retcode = write(sock, nameAndSize, 50);  //file_name 전송 
+
+
+
+
+			while (1){
+
+				numread = fread(buf, 1, 64, fp); // fp파일을 읽어서 buf에 저장하고 그 저장한 바이트 만큼 numread에 int값으로 저장 
+				retcode = write(sock, buf, numread);
+
+				sumfileSize += numread; //byte 수를계속 더해주는것. 현재까지 보낸 파일의 크기를 나타냄. 
+
+				percent = ((double)sumfileSize / fileSize) * 100;
+
+				//출력 포멧.  \r 은 덮어쓰기,  fflush(stdout) 은 덮어쓰기용 함수  
+
+				printPercent(percent, sumfileSize, fileSize, &count);
+
+				count++; //count를 계속 초기화해주면서 실시간 상태 나타냄. 
+
+				if (retcode <= -1) {
+					printf("client: sendto failed: %d\n", errno); exit(0);
+				}
+				if (retcode == 0){  // 더이상 보낼것이 없으면 종료.
+
+					retval = read(sock, msg, 50);
+					if (strcmp(msg, strFinish) == 0){
+
+						printf("\r[**********] (100% ) %dMB/%dMB\n", fileSize / 1000000, fileSize / 1000000);
+						printf("strcmp : %d\n", strcmp(msg, strFinish));
+
+						count = 0;
+						printf(" File upload OK !! \n ");
+
+
+						break;
+					}
+
+				}
+			}
+		}
+
+	}
+
+	close(sock);
+}
+
+void printPercent(double percent, int sumfileSize, int fileSize, int* count)
+{
+	if (percent<10 && *count == 10000){
+		printf("[*         ] ( %d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<20 && *count == 10000){
+		printf("[**        ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<30 && *count == 10000){
+		printf("[***       ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<40 && *count == 10000){
+		printf("[****      ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<50 && *count == 10000){
+		printf("[*****     ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<60 && *count == 10000){
+		printf("[******    ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<70 && *count == 10000){
+		printf("[*******   ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<80 && *count == 10000){
+		printf("[********  ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else if (percent<100 && *count == 10000){
+		printf("[********* ] (%d% ) %dMB/%dMB\r", (int)percent, sumfileSize / 1000000, fileSize / 1000000); fflush(stdout);
+		*count = 0;
+	}
+	else {}
+
+
+}
---
+++ Server/server.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	struct servent *servp;
+	struct sockaddr_in my_addr, client_addr;
+	int request_sock, new_sock;
+	int nfound, fd, maxfd, bytesread;
+	fd_set rmask, mask;
+	static struct timeval timeout = { 5, 0 }; /* 5 seconds */
+
+	int sockid = 0, nread = 0, addrlen = 0, retval = 0;
+	int retcode;
+	size_t numread;       //소켓에서 받아온 데이터 크기를 저장하는 변수들
+
+
+	char msg[255];
+	char buf[64];
+	char file_name[50];
+	char strPut[10] = "put";        // client 명령 비교하기 위해 (strcmp 사용)
+	char strGet[10] = "get";
+	char strClose[10] = "close";
+	char strACK[10] = "ACK";
+	char strEmpty[10] = "Empty";
+	int fileSize = 0;              //파일의 전체 사이즈를 나타내는 변수
+	int BUFSIZE = 63;
+	int sumfileSize = 0;           //받아오거나 보내는 파일의 현재 사이즈를 나타내는 변수
+	int secondCount = 0;           // 1초를 나타내기 위한 count 단위
+	double percent;
+	char file_size[50];
+	int start = 0;
+	int thiscount = 0;
+	int sizeCount = 0;
+	char nameAndSize[50];
+	char strFinish[50] = "finish";
+
+	if (argc != 2) {
+		(void)fprintf(stderr, "usage: %s service|port\n", argv[0]);
+		exit(1);
+	}
+	if ((request_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+	if (isdigit(argv[1][0])) {
+		static struct servent s;
+		servp = &s;
+		s.s_port = htons((u_short)atoi(argv[1]));
+	}
+	else if ((servp = getservbyname(argv[1], "tcp")) == 0) {
+		fprintf(stderr, "%s: unknown service\n", "tcp");
+		exit(1);
+	}
+	memset((void *)&my_addr, 0, sizeof my_addr);
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_addr.s_addr = INADDR_ANY;
+	my_addr.sin_port = servp->s_port;
+	if (bind(request_sock, (struct sockaddr *)&my_addr, sizeof my_addr) < 0) {
+		perror("bind");
+		exit(1);
+	}
+	if (listen(request_sock, SOMAXCONN) < 0) {
+		perror("listen");
+		exit(1);
+	}
+	FD_ZERO(&mask);
+	FD_SET(request_sock, &mask);
+	maxfd = request_sock;
+
+	while (1){
+		/* a new connection is available on the connetion socket */
+		addrlen = sizeof(client_addr);
+		new_sock = accept(request_sock, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen);
+		if (new_sock < 0) {
+			perror("accept");
+			exit(1);
+		}
+		printf("connection from host %s, port %d, socket %d\n",
+			inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),
+			new_sock);
+		FD_SET(new_sock, &mask);
+		if (new_sock > maxfd) maxfd = new_sock;
+		FD_CLR(request_sock, &rmask);
+		break;
+
+
+	}
+
+	while (1){
+		printf("wait message!\n");
+		nread = read(new_sock, msg, 20);
+		// client 에서 보낸 command명령 받음
+
+		////////////////////////////////client put
+		sizeCount = 0;
+		thiscount = 0;
+		fileSize = 0;
+
+		bzero(file_size, 50);
+		bzero(file_name, 50);
+
+		if (!strcmp(strPut, msg)){
+
+			nread = read(new_sock, msg, 50);
+			//recive file_name & file_Size  client에서 파일이름 받아옴
+
+			strcpy(nameAndSize, msg);
+			for (start = 0; start<strlen(nameAndSize); start++){
+				if (nameAndSize[start] == '.') thiscount = start;
+
+			}
+
+			for (start = 0; start<thiscount; start++){
+				file_name[sizeCount] = nameAndSize[start];
+				sizeCount++;
+			}
+			file_name[thiscount] = '\0';
+			sizeCount = 0;
+
+			for (start = thiscount + 1; start<strlen(msg); start++){
+				file_size[sizeCount] = nameAndSize[start];
+				sizeCount++;
+			}
+
+			printf("file name : %s \n", file_name);
+
+			fileSize = atoi(file_size);
+			//recive fileSize    client에서 파일 사이즈 받아옴                    
+
+			printf("file size : %d \n", fileSize);
+
+
+			FILE* fp;                   //파일 지정 후 받아온 파일이름으로 오픈.
+			fp = fopen(file_name, "w+");   // w+ 쓰기+읽기모드로 파일이 없는 경우에 생성함.
+
+			percent = 0;            // 출력인자 초기화
+			sumfileSize = 0;
+			retval = 1;
+			//client put start
+
+			printf("Transfer status: recv[%s] [ 0% , 0MB/%dMB]\n", file_name, fileSize / 1000000); //출력포멧. 0%인 경우.
+			strcpy(msg, strEmpty);
+			while (1){
+
+				retval = read(new_sock, buf, 64);
+				sumfileSize += retval; //그것들을 현재받는파일의 사이즈에 더해준다 // 현재 크기를 나타냄.
+
+				fwrite(buf, 1, retval, fp);
+
+				//이건 받아온 버퍼를 fp라는 FILE 포인터 변수에 저장하는데. 이것이 파일로 써지는것.
+				percent = ((double)sumfileSize / fileSize) * 100; // 전체크기로 나눠서 퍼센트로 나타냄.
+
+				if (secondCount == 80000){ // 약 1초가 되는경우에 출력함.
+					printf("Transfer status: recv[%s] [%d% , %dMB/%dMB]\n", file_name, (int)percent, sumfileSize / 1000000, fileSize / 1000000);
+					secondCount = 0;
+
+				}
+
+				if (sumfileSize == fileSize){  // 더이상 보낼 것이 없으면 종료한다.
+					printf(" receive file : %d\n ", sumfileSize);
+					fclose(fp);
+
+					printf("Transfer status: recv[%s] [100% , %dMB/%dMB]\n", file_name, fileSize / 1000000, fileSize / 1000000);
+
+					printf(" File download OK !!\n"); //완료시 출력
+					retval = write(new_sock, strFinish, 50);
+					break;
+				}
+
+				secondCount++; // 시간을 나타내기위한 단위 80000이되면 다시 초기화됨.
+
+
+			}  //close while
+
+			strcpy(msg, strEmpty);
+
+		}
+		////////////////////////////////client put
+	}
+	printf("server: end of file on %d\n", fd);
+	FD_CLR(fd, &mask);
+
+
+}
